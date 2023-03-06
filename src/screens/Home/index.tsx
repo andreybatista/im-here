e7ecad3c38b492a3e30a823e04b24e9675f24009
andreybatista <andreybatista131@gmail.com>
2023-03-06 15:21:13 -0300

    add FlatList for participant

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,31 @@
-import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, FlatList } from "react-native";
 
 import { Participant } from "../../components/Participant";
 
 import { styles } from "./styles";
 
 export function Home() {
+  const participants = [
+    "Andrey",
+    "Noeme",
+    "Mini Noeme",
+    "Mini Andrey",
+    "Andrey2",
+    "Noeme2",
+    "Mini Noeme2",
+    "Mini Andrey2",
+    "Andrey3",
+    "Noeme3",
+    "Mini Noeme3",
+    "Mini Andrey3",
+  ]
 
   function handleParticipantAdd() {
     console.log('click')
+  }
+
+  function handleParticipantRemove(name: string) {
+    console.log(`click para remove o participante ${name}`)
   }
 
   return (
@@ -34,9 +52,24 @@ export function Home() {
         </TouchableOpacity>
       </View>
 
-      <Participant name="Andrey" />
-      <Participant name="Noeme" />
-      <Participant name="Mini Noeme" />
+      <FlatList
+        keyExtractor={item => item}
+        data={participants}
+        renderItem={({ item }) => (
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove(item)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={()=>(
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participante a sua lista de presença.
+          </Text>
+        )}
+      />
+
 
     </View>
   )
